bd3a74835227b3c4273bcaf99abf0265eee7e158
Julius Frank <11juliashfrank97@gmail.com>
2022-11-24 10:09:40 -0500

    getting rid of testing defaults

--- config.py
+++ config.py
@@ -1,16 +1,11 @@
 POLL_PASS_THRESHOLD = 2 / 3  # % of the people who voted must have voted yes
-POLL_DURATION = 3  # Time before poll is closed, in seconds
+POLL_DURATION = 24*60*60  # Time before poll is closed, in seconds
 POLL_YES_EMOJI = "✅"  # emoji to use for the poll's "yes" option
 POLL_NO_EMOJI = "❌"  # emoji to use for the poll's "no" option
 TOKEN_FILE_NAME = ".TOKEN"  # file containing the bot's token
-WAIT_TIME_BETWEEN_CHECKS = 1  # Time between checks for poll results, in seconds
+WAIT_TIME_BETWEEN_CHECKS = 10*60  # Time between checks for poll results, in seconds
 DELETE_NOTIFICATIONS_AFTER = 5  # Time to wait before deleting notifications, in seconds
-MAX_IMAGE_SIZE = (
-    320**2
-)  # Max area of image in pixels, set by discord so be careful changing this
-MAX_IMAGE_FILE_SIZE = (
-    256000  # Max file size of image, set by discord so be careful changing this
-)
-TEMP_IMAGE_FILE_NAME = (
-    "adding_image_temp"  # Name of temporary image file for adding emojis and stickers
-)
+MAX_IMAGE_SIZE = 320**2  # Max area of image in pixels, set by discord so be careful changing this
+MAX_IMAGE_FILE_SIZE = 256000  # Max file size of image, set by discord so be careful changing this)
+TEMP_IMAGE_FILE_NAME = "adding_image_temp"  # Name of temporary image file for adding emojis and stickers
+
